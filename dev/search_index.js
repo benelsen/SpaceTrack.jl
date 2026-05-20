c3468f3cc1ed6e9ec3062af2af6ea33c42c293df
Documenter.jl <documenter@juliadocs.github.io>
2026-05-20 18:10:52 +0000

    build based on 5e91762

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpaceTrack","category":"page"},{"location":"#SpaceTrack","page":"Home","title":"SpaceTrack","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpaceTrack.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpaceTrack]","category":"page"}]
+[{"category":"section","location":"#SpaceTrack","page":"Home","text":"Documentation for SpaceTrack.\n\n","title":"SpaceTrack"}]
 }
